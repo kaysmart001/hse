b5b4402156c26fac2c416313630079c894321c82
kaysmart001 <luwoyekayode99@gmail.com>
2023-01-30 12:35:54 +0100

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'Lima5@Konekthing');
-define('DB_NAME', 'hse');
+define('DB_HOST', 'sql311.epizy.com');
+define('DB_USER', 'epiz_33459705');
+define('DB_PASS', 'hvZgW4p2guu7');
+define('DB_NAME', 'epiz_33459705_onlinetest_db');
